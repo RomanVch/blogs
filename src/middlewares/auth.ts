94cdd2bc2bf17adff0b519a8b33eec433edffd69
r.lobov <romavyacheslavovich@gmail.com>
2022-12-14 22:02:14 +0300

    fix

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -6,16 +6,19 @@ export const auth = (req:Request, res:Response, next:NextFunction) => {
 
 
     if(req.headers.authorization){
-
+        const typeAuth = (req.headers.authorization).split(' ')[0]
+        if(typeAuth === 'Basic'){
         const b64auth = (req.headers.authorization).split(' ')[1]
         const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
         let index = auth.findIndex(el => el.login === login);
-
         if (auth[index] && login && password && login === auth[index].login && password === auth[index].password) {
             return next()
         }
             res.status(401).send('Authentication required.');
-    } else {
+        } else {
+            res.status(400).send('Authentication required.')
+        }
+        } else {
         res.status(401).send('Authentication required.');
     }
 
