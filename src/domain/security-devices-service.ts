9eb9eb4d73c2c3b5468c2c82668d774d37a77910
r.lobov <romavyacheslavovich@gmail.com>
2023-02-03 17:48:02 +0300

    start

--- src/domain/security-devices-service.ts
+++ src/domain/security-devices-service.ts
@@ -1,13 +1,5 @@
-
-import {BlogSimpleIdT, CorrectPostT, DataForNewPostT, PostMongoIdT, PostSimpleIdT, PostT} from "../repository/types";
-import {postInDbRepository} from "../repository/post-in-db-repository";
-import {PostsQueryT} from "../routers/postRouter";
-import {blogsDbRepository} from "../repository/blogs-db-repository";
-import {blogsService} from "./blog-service";
-import {EndRouterT} from "../routers/blogsRouter";
 import {usersService} from "./users-service";
 import {ObjectId} from "mongodb";
-import {getDeviceSession} from "../utils/getDeviceSession";
 
 export const securityDevicesService = {
     async delOtherDevicesSession(userId:string,userAgent:string, ip:string){
